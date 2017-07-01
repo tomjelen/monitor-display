61f73d1fb94657ce38871503e8463512acd0315b
Tom Jelen <tom@jelen.dk>
2017-07-01 23:58:06 +0200

    retry a few times if the monitor fails to report status

--- app_tests.py
+++ app_tests.py
@@ -1,6 +1,7 @@
 # pylint: disable=C0103
 import unittest
 from unittest import mock
+from unittest.mock import patch
 from unittest.mock import ANY
 import requests_mock
 import app
@@ -33,7 +34,8 @@ class AppTests(unittest.TestCase):
         fake_display.show_failure.assert_called_with()
 
     @requests_mock.mock()
-    def test_displays_error_when_monitor_is_dead(self, fake_requests):
+    @patch('time.sleep', return_value=None)  # Instant sleeps
+    def test_displays_error_when_monitor_is_dead(self, fake_requests, fake_sleep):
         fake_requests.get('http://example.org/status',
                           json={'a': 'b'},
                           status_code=500)
@@ -43,8 +45,17 @@ class AppTests(unittest.TestCase):
         app.display_current_monitor_status(monitor, fake_display)
         fake_display.show_general_failure.assert_called_with(ANY)
 
-    def test_will_retry_on_monitor_faliure(self):
-        pass
+    @requests_mock.mock()
+    @patch('time.sleep', return_value=None)  # Instant sleeps
+    def test_will_retry_on_monitor_failure(self, fake_requests, fake_sleep):
+        fake_requests.get('http://example.org/status',
+                          json={'a': 'b'},
+                          status_code=500)
+
+        fake_display = mock.Mock(spec=display)
+
+        app.display_current_monitor_status(monitor, fake_display)
+        self.assertGreater(fake_requests.call_count, 1)
 
 
 if __name__ == '__main__':
--- monitor.py
+++ monitor.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import time
 import requests
 
 MONITOR_URL = 'http://example.org/status'
@@ -9,13 +10,19 @@ class MonitorNotAvailableError(Exception):
     pass
 
 
-def query_status():
+def query_status(retry_attempts=0):
     resp = requests.get(MONITOR_URL)
-
     if resp.status_code == 200:
         return report(resp.json())
     else:
-        raise MonitorNotAvailableError('The monitor seems to be dead :(')
+        # back off if the server is throwing errors
+        if retry_attempts < 5:
+            print('retrying..')
+            time.sleep(5 * retry_attempts)
+            retry_attempts += 1
+            return query_status(retry_attempts)
+        else:
+            raise MonitorNotAvailableError('The monitor seems to be dead :(')
 
 
 def report(monitor_result):
